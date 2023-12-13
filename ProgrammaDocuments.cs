f138d6900566aa580c3b247b7fb2a2393b90abb9
Marcelino Bistervelts <bima@roc-teraa.nl>
2023-12-13 11:12:05 +0100

    weer een opmerking

---
+++ ProgrammaDocuments.cs
@@ -0,0 +1,8 @@
+class TestenGithub
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hallo fijn dat het gelukt is om lokale cs bestand te koppelen aan online Github-omgeving");
+        Console.ReadLine();
+    }
+}
